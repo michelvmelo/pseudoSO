50a1755ae4910940b0080c0132bae5ae1c91ec4b
Jean Pierre <tavares1988@live.com>
2018-10-12 16:10:41 -0300

    modulos criados

---
+++ Implementação/source/escalonador.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+#include "../headers/escalonador.h"
---
+++ Implementação/source/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	/*char *process_input;	Aponta para arquivo contendo informações do processo*/
+	/*char *instruc_input;	Aponta para arquivo contendo informações detalhada do "sistema"*/
+
+	if(argc < 3){
+		printf("Qtd de arg. invalido.\n");
+		printf("Entre com: ./dispacher <processes.txt> <file.txt>\n");
+		return -1;
+	}
+
+	/*process_input = argv[1];
+	instruc_input = argv[2];*/
+	return 0;
+}
