e90cf3f21e863995cab1d3c566fa604da38ded7b
Paulo Passos <cunha.passos@gmail.com>
2018-11-24 20:10:43 -0200

    Carregando processos e informações do sistema

--- Implementação/Python/main.py
+++ Implementação/Python/main.py
@@ -4,7 +4,7 @@ def main():
 
     if len(sys.argv) > 2:
         processos = sys.argv[1]
-        arquivos = sys.argv[2]
+        operacoes = sys.argv[2]
     else:
         print ("Para rodar corretamente o sistema digite: main.py + 'nome do arquivo de processos' + 'nome do arquivo de arquivos'")
 
